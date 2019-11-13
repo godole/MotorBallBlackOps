ee6c7c606b3e45076127629c0e0552f3d868ef3f
Godole <godole199@naver.com>
2019-11-14 01:25:37 +0900

    Save

---
+++ Assets/Scripts/Machine/MachineInputData.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class MachineInputData
+{
+    public enum EMachineExtendStat
+    {
+        BatteryCapacity,
+        BatteryConsume,
+        BatteryUseConsume,
+        OverdriveLimit,
+        MaximumSpeed,
+        AcceleratingForce,
+        Overdrive,
+        Speed,
+        Acceleration,
+        Weight,
+        Handling,
+        BrakingForce,
+        Endurance,
+        DefensePower,
+        Damage,
+        SprayPenetration,
+        AttackSpeed,
+        ReloadSpeed,
+        ChargeTime,
+        LockOnTime
+    }
+
+    public enum EMachineBaseStat
+    {
+        Type,
+        Grade,
+        BatteryEfficient,
+        EnginePower,
+        Mobility,
+        Armor,
+        FirePower
+    }
+
+    Dictionary<EMachineExtendStat, int> m_ExtendData;
+    Dictionary<EMachineBaseStat, int> m_BaseStats;
+
+    public void SetCoefficientData(EMachineExtendStat type, int data)
+    {
+        m_ExtendData[type] = data;
+    }
+
+    public void SetMachineBaseStat(EMachineBaseStat type, int data)
+    {
+        m_BaseStats[type] = data;
+    }
+
+    public void CalculateMachineStat(MachineBase machine)
+    {
+
+    }
+}
